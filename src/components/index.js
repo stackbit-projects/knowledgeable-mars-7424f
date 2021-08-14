b6039f7fca00b7604457a0d358aef8ce8bd1ff60
Stackbit Code Editor <projects@stackbit.com>
2021-08-14 04:40:32 +0000

    index.js: changed

--- src/components/index.js
+++ src/components/index.js
@@ -14,7 +14,6 @@ import Icon from './Icon';
 import Layout from './Layout';
 import SectionActions from './SectionActions';
 import TeamSection from './TeamSection';
-import Sect
 
 export {
     Action,
